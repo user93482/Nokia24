964d855e4377e0aa654a37f36bd5d85fcb85042c
user93482 <106318293+user93482@users.noreply.github.com>
2024-05-25 10:57:44 +0200

    Update main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,46 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+abc = "abcdefghijklmnopqrstuvwxyz"
+nums = '0123456789'
 
-print(input)
+with open('./input.txt', 'r') as f:
+    input = f.read()
+
+input = input.lower()
+
+candidates = ['']
+j = 0
+
+for i in input:
+    if i =='\n':
+        candidates.append('')
+        j+=1
+    elif i in abc or i in nums:
+        candidates[j] += i
+
+palindromes = []
+uniques = []
+
+qualified = []
+
+for i in range(len(candidates)):
+    j=0
+    uniques.append([])
+    while j < int(len(candidates[i])/2)+1:
+        if candidates[i][j] not in uniques[i]:
+            uniques[i].append(candidates[i][j])
+            
+        if candidates[i][j] != candidates[i][-j-1]:
+
+            
+            break
+        j+=1
+    
+    if j == int(len(candidates[i])/2)+1:
+        palindromes.append(candidates[i])
+        qualified.append('YES')
+        uniques[i] = len(uniques[i])
+    else:
+        qualified.append('NO')
+        uniques[i] = -1
+
+for i in range(len(qualified)):
+    print(f"{qualified[i]}, {uniques[i]}")
